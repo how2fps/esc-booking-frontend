81067c891b5ce85218550c0db49078ddc50cd8a2
LuckyMcLucks <80624532+LuckyMcLucks@users.noreply.github.com>
2025-07-12 15:13:03 +0800

    Added Frontend Template for Login, Hotel Listing and Hotel Details

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,9 @@ import { Route, Routes } from "react-router-dom";
 import { HomePage } from "./features/pages/home/HomePage";
 import { SignupForm } from "./features/pages/signup-form/SignupForm";
 import Home from "./features/pages/searchpage/home";
+import HotelDetail from "./features/pages/hotel-detail/hotel-detail";
+import Listings from "./features/pages/hotel-list/hotel-list";
+import Login from "./features/pages/login/page";
 const AppRoutes: React.FC = () => {
        return (
 
@@ -25,6 +28,18 @@ const AppRoutes: React.FC = () => {
                      <Route
                             path="/dashboard"
                             element={<Home />}
+                     />
+                     <Route
+                            path="/hotel"
+                            element={<HotelDetail />}
+                     />
+                     <Route
+                            path="/hotel-list"
+                            element={<Listings />}
+                     />
+                     <Route
+                            path="/login"
+                            element={<Login />}
                      />
               </Routes>
 
