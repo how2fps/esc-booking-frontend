14051222af7369c99e00349f9204c97260aeccea
finnyjh <68309914+how2fps@users.noreply.github.com>
2025-07-20 17:49:44 +0800

    need to convert api to set and store it in cache

--- src/features/pages/hotel-listings/FilterCheckbox.tsx
+++ src/features/pages/hotel-listings/FilterCheckbox.tsx
@@ -1,12 +1,26 @@
 import * as Icon from "phosphor-react";
 import "rc-slider/assets/index.css";
+import { useState } from "react";
 
 type FilterCheckboxProps = {
        header: string;
        options: string[];
+       setFilters: React.Dispatch<React.SetStateAction<string[]>>;
 };
 
-export const FilterCheckbox = ({ header, options }: FilterCheckboxProps) => {
+export const FilterCheckbox = ({ header, options, setFilters }: FilterCheckboxProps) => {
+       const [selectedOptionsSet, setSelectedOptionsSet] = useState<Set<string>>(new Set());
+       const handleCheckboxChange = (option: string) => {
+              const newSet = new Set(selectedOptionsSet);
+              if (newSet.has(option)) {
+                     newSet.delete(option);
+              } else {
+                     newSet.add(option);
+              }
+              setSelectedOptionsSet(newSet);
+              setFilters(Array.from(newSet));
+       };
+
        return (
               <div className="filter-amenities mt-7">
                      <div className="heading6">{header}</div>
@@ -21,8 +35,8 @@ export const FilterCheckbox = ({ header, options }: FilterCheckboxProps) => {
                                                                type="checkbox"
                                                                name={option}
                                                                id={option}
-                                                               checked={true}
-                                                               onChange={() => {}}
+                                                               checked={selectedOptionsSet.has(option)}
+                                                               onChange={() => handleCheckboxChange(option)}
                                                         />
                                                         <Icon.CheckSquare
                                                                size={20}
