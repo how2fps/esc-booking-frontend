fedf1cfa479dc6e1538483a1e368d276e7109daf
how2fps <hearthstonesigh@gmail.com>
2025-07-24 22:37:17 +0800

    moved header adn footer to app.tsx, one single source of header/footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
-import './styles/styles.scss'
+import Footer from "./features/components/Footer/Footer";
+import HeaderOne from "./features/components/Header/Header";
 import AppRoutes from "./routes";
+import "./styles/styles.scss";
 
 function App() {
        return (
               <BrowserRouter>
+                     <HeaderOne />
                      <AppRoutes />
+                     <Footer />
               </BrowserRouter>
        );
 }
